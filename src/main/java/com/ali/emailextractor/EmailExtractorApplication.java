9da3275b7f4f9df577f825f2bbf05eed0a6fa702
meraihan <sayedmahmudraihan@gmail.com>
2020-12-04 12:13:09 +0600

    initial structure

---
+++ src/main/java/com/ali/emailextractor/EmailExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.ali.emailextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailExtractorApplication.class, args);
+	}
+
+}
